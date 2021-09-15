0b627525df71d4a8cd2f25f7feb04465f8494740
muryo13 <muryo13@gmail.com>
2021-09-16 08:34:32 +0900

    棒グラフ表示追加

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ var app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http)
 const port = process.env.PORT || 3000;
+const date = new Date();
 
 var participantNum = 0;
 var iineNum = 0;
+var discreteIine = 0;
 
 app.use(express.static(__dirname + '/public'));
 app.get('/', function (req, res) {
@@ -19,6 +21,7 @@ function onConnection(socket) {
     socket.on('message', function (msg) {
         // console.log(msg);
         iineNum++;
+        discreteIine++;
         socket.emit("message", "iineNum," + iineNum);
         socket.broadcast.emit("message", "iineNum," + iineNum);
     });
@@ -36,5 +39,11 @@ function onConnection(socket) {
 }
 
 io.on('connection', onConnection);
+
+setInterval(function () {
+    console.log("chart," + Date.now() + "," + discreteIine);
+    io.sockets.emit("message", "chart," + Date.now() + "," + discreteIine);
+    discreteIine = 0;
+}, 5000);
 
 http.listen(port, () => console.log('listening on port ' + port));
--- public/main.js
+++ public/main.js
@@ -1,6 +1,57 @@
 'use strict';
 
 (function () {
+    var chartData = {x:0,y:0};
+    const config = {
+        type: 'bar',
+        data: {
+            datasets: [
+                {
+                    label: 'データセット 1',
+                    backgroundColor: 'rgba(255, 99, 132, 0.5)',
+                    borderColor: 'rgb(255, 99, 132)',
+                    borderDash: [8, 4],
+                    fill: true,
+                    data: []
+                },
+                // {
+                //     label: 'データセット 2',
+                //     backgroundColor: 'rgba(54, 162, 235, 0.5)',
+                //     borderColor: 'rgb(54, 162, 235)',
+                //     cubicInterpolationMode: 'monotone',
+                //     fill: true,
+                //     data: []
+                // }
+            ]
+        },
+        options: {
+            plugins: {
+                streaming: {
+                    duration: 300000
+                }
+            },
+            scales: {
+                x: {
+                    type: 'realtime',
+                    realtime: {
+                        delay: 1000,
+                        refresh: 5000,
+                        onRefresh: chart => {
+                            chart.data.datasets[0].data.push({
+                                x: Date.now(),
+                                y: chartData[2]
+                            });
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    const myChart = new Chart(
+        document.getElementById('myChart'), config
+    );
+
     var socket = io();
     var num = 0;
 
@@ -15,9 +66,11 @@
                 document.getElementById('allIineNum').innerText = words[1];
             } else if (words[0] == 'participantNum') {
                 document.getElementById('participantNum').innerText = words[1];
+            } else if (words[0] == 'chart') {
+                console.log(words);
+                chartData = words;
             }
-        });
-
+        })
     });
 
     document.addEventListener('DOMContentLoaded', function (e) {
